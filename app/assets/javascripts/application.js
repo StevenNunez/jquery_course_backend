d798c0cfebfb2b5df8e3ea2dac704212e034dbee
Steven Nunez <steven.nunez@gmail.com>
2016-01-21 11:27:35 -0500

    Port Old App

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,22 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(function(){
+  $('.courses').on('click', '.course', function(){
+    $(this).find('.details').slideToggle();
+  });
+
+  $('#add-new-course').on('submit', function(event){
+    event.preventDefault();
+    var courseName = $(this).find('#course-name').val();
+    var courseDetails = $(this).find('#course-details').val();
+    var template = `<li class="course">
+      ${courseName}
+      <div class="details">
+        ${courseDetails}
+      </div>
+    </li>`
+    $('.courses').append(template);
+    this.reset();
+  });
+});
