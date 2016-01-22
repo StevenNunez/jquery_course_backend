54f399a24d5371beb9015657af59ca5dbe949de0
Steven Nunez <steven.nunez@gmail.com>
2016-01-22 11:45:36 -0500

    Blow their minds

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,17 +22,64 @@ function courseTemplate(name, details){
       </div>
     </li>`
 }
+
+function Course(id, name, details){
+  this.id = id;
+  this.name = name;
+  this.details = details;
+}
+
+Course.all = function(callback){
+  // Get the json
+  $.getJSON('/courses', function(coursesJson){
+    // Convert the responses to an array of Course Objects
+    var courses = coursesJson.map(function(course){
+      return Course.newFromJSON(course)
+    });
+    // Call the function (callback), with the array of Course Objects
+    //   as the argument
+    callback(courses);
+  });
+}
+
+Course.newFromJSON = function(json){
+  return new Course(json.id, json.name, json.details);
+}
+
+Course.prototype.toTemplate = function(){
+  return `<li class="course">
+      ${this.name}
+      <div class="details">
+        ${this.details}
+      </div>
+    </li>`
+};
+
+// toTemplate
+// save
+// Course.all
+
 $(function(){
   $('.courses').on('click', '.course', function(){
     $(this).find('.details').slideToggle();
   });
 
-  $.getJSON('/courses', function(courses){
+  var appendToPage = function(courses){ // This will be Course objects
     var remoteCourses = courses.map(function(course){
-      return courseTemplate(course.name, course.details)
+      return course.toTemplate();
     });
     $('.courses').append(remoteCourses);
-  });
+  }
+
+  Course.all(appendToPage);
+
+  // $.getJSON('/courses', function(courses){
+  //   var remoteCourses = courses.map(function(course){
+  //     var courseObject = new Course(course.id, course.name, course.details);
+  //     return courseObject.toTemplate();
+  //   });
+  //   $('.courses').append(remoteCourses);
+  // });
   $('#add-new-course').on('submit', function(event){
     event.preventDefault();
     var courseName = $(this).find('#course-name').val();
